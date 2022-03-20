b890e1239bf3738290130e65b07f4aa3cdd1401e
yasseryahiia <yasser.0.yahia@protonmail.com>
2022-03-20 15:47:59 +0200

    building the navbar event

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -22,15 +22,15 @@
  * Define Global Variables
  * 
 */
-
-
+    const sections = document.querySelectorAll("section");
+    const scLenght = sections.length ;
+    const navbar = document.querySelector(".navbar_menu");
+    const ul = document.querySelector("#navbar__list");   
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
-
-
+*/  
 
 /**
  * End Helper Functions
@@ -39,10 +39,8 @@
 */
 
 // build the nav
-
-
+    
 // Add class 'active' to section when near top of viewport
-
 
 // Scroll to anchor ID using scrollTO event
 
@@ -54,9 +52,23 @@
 */
 
 // Build menu 
+    document.addEventListener("DOMContentLoaded" , function(){
+        const fragment = document.createDocumentFragment();
+        for (const section of sections){
+            const li = document.createElement("li");
+            const anchor = document.createElement("a");
+            anchor.href = `#${section.id}` ;
+            anchor.classList.add("menu__link");
+            anchor.textContent = section.dataset.nav;
+            li.appendChild(anchor);
+            fragment.appendChild(li);
+            console.log(anchor);
+        }
+        ul.appendChild(fragment);
 
+    });
 // Scroll to section on link click
-
+    
 // Set sections as active
 
 
